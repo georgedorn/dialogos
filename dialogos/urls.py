420c8b62f1e6d276f1115b79116885bbd2ff6304
George Dorn <georgedorn@gmail.com>
2012-06-28 01:32:26 -0700

    fix for non-integer primary keys in urls

--- dialogos/urls.py
+++ dialogos/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import url, patterns, include, handler404, handle
 
 
 urlpatterns = patterns("dialogos.views",
-    url(r"^comment/(?P<comment_id>.+)/edit/$", "edit_comment",
+    url(r"^comment/(?P<comment_id>.+?)/edit/$", "edit_comment",
         name="edit_comment"),
-    url(r"^comment/(?P<content_type_id>.+)/(?P<object_id>.+)/$", "post_comment",
-        name="post_comment"),
-    url(r"^comment/(?P<comment_id>.+)/delete/$", "delete_comment",
+    url(r"^comment/(?P<comment_id>.+?)/delete/$", "delete_comment",
         name="delete_comment"),
+    url(r"^comment/(?P<content_type_id>.+?)/(?P<object_id>.+?)/$", "post_comment",
+        name="post_comment"),
 )
